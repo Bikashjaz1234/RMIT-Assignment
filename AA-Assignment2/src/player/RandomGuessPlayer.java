ea35a759c495427d4a016260e69a3cadad820dc5
tx99 <andrew039@gmail.com>
2017-05-28 10:54:44 +1000

    Part A rewrite

--- AA-Assignment2/src/player/RandomGuessPlayer.java
+++ AA-Assignment2/src/player/RandomGuessPlayer.java
@@ -1,8 +1,6 @@
 package player;
 
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 import ship.Ship;
 import world.World;
@@ -12,80 +10,152 @@ import world.World;
  * Please implement this class.
  *
  * @author Youhan, Jeffrey for startup code.
- * @author Harold for Implement
  */
 public class RandomGuessPlayer implements Player{
 
-	int rowNum = 0;
-	int clnNum = 0;
-	boolean isHex = false;
-	boolean[][] isused;
-	boolean makeGuessFlg = true;
-	int shipNum = 5;
-	
-	private class myShip{
-		boolean[] isDead = {false, false, false, false, false};
-	}
-	
+    World world;
+    int row;
+    int col;
+    int maxGuesses = -1;
+    //ArrayList<Guess> guesses = new ArrayList<>();
+    ArrayList<World.ShipLocation> shipLoc = new ArrayList<>();
+    HashSet<Guess> guesses = new HashSet<>();
+    ArrayList<Guess> tmp = new ArrayList<>();
+
+
     @Override
-    public void initialisePlayer(World world) {
-        // To be implemented.
-    	 this.rowNum = world.numRow;
-    	 this.clnNum = world.numColumn;
-    	 this.isHex = world.isHex;
-    	 this.isused = new boolean[rowNum][clnNum];
-    	 
+    public void initialisePlayer(World world)
+    {
+        this.world = world;
+        maxGuesses = world.numRow * world.numColumn;
+
+        // ship locations
+        for (int i = 0; i < world.shipLocations.size(); i++)
+        {
+            shipLoc.add(world.shipLocations.get(i));
+        }
+
+
+
+        for (int i = 0; i < shipLoc.size(); i++)
+        {
+
+        }
+
+        int startRow = 0;
+        int startCol = 0;
+
+        for (startRow = 0; startRow < world.numRow; startRow++)
+        {
+            for (startCol = 0; startCol < world.numColumn; startCol++)
+            {
+                Guess guess = new Guess();
+                guess.row = startRow;
+                guess.column = startCol;
+
+                guesses.add(guess);
+            }
+        }
+
+
+        tmp.addAll(guesses);
+
+        /*for (int i = 0; i < tmp.size(); i++)
+        {
+            System.out.println(tmp.get(i).row + ":" + tmp.get(i).column);
+            System.out.println();
+        }
+
+        System.out.println("guesses size: " + guesses.size());*/
+
+
+
+
     } // end of initialisePlayer()
 
     @Override
-    public Answer getAnswer(Guess guess) {
-        // To be implemented.
+    public Answer getAnswer(Guess guess)
+    {
+        Answer answer = new Answer();
+        ArrayList<World.Coordinate> cdns;
+        World.Coordinate cdn;
+        Ship ship;
+        for (int i = 0; i < this.shipLoc.size(); i++) {
+            cdns = this.shipLoc.get(i).coordinates;
+            ship = this.shipLoc.get(i).ship;
+            for (int m = 0; m < cdns.size(); m++) {
+                cdn = cdns.get(m);
+                if (cdn.row == guess.row && cdn.column == guess.column) {
+                    answer.isHit = true;
+                    cdns.remove(m);
+                    // check whether current ship is sunk
+                    if (cdns.isEmpty()) {
+                        answer.shipSunk = ship;
+                        this.shipLoc.remove(i);
+                        return answer;
+                    }
+                }
+            }
+        }// end for loop
 
-        // dummy return
-        return null;
+        return answer;
     } // end of getAnswer()
 
 
     @Override
-    public Guess makeGuess() {
-        // Setup X and Y.
-    	int x = -1;
-    	int y = -1;
-    	// Loop for make a guess, if the guess is used, keep looping.
-    	while(makeGuessFlg){
-    		// Generate random number from 0 to 10
-    		x = (int)(Math.random()*10);
-    		y = (int)(Math.random()*10);
-    		System.out.println("the X is: " + x);
-    		System.out.println("the Y is: " + y);
-    		// Check it used or not.
-    		if (isused[x][y] == false){
-    			makeGuessFlg = false;
-    		}
-    	}
-    	this.isused[x][y] = true;
-    	Guess serverGuess = new Guess();
-    	serverGuess.row = x;
-    	serverGuess.column = y;
-    	System.out.println("the X is: " + x);
-		System.out.println("the Y is: " + y);
-        // dummy return
-        return serverGuess;
+    public Guess makeGuess()
+    {
+        int guessCount = 0;
+        Guess guessLocal = new Guess();
+
+        int index = generateRandomNum("guess");
+
+        guessLocal = tmp.get(index);
+
+        tmp.remove(index);
+
+        System.out.println(guessLocal.toString());
+
+        return guessLocal;
     } // end of makeGuess()
 
 
     @Override
-    public void update(Guess guess, Answer answer) {
+    public void update(Guess guess, Answer answer)
+    {
         // To be implemented.
     } // end of update()
 
 
     @Override
-    public boolean noRemainingShips() {
+    public boolean noRemainingShips()
+    {
         // To be implemented.
 
         // dummy return
         return true;
     } // end of noRemainingShips()
+
+    public int generateRandomNum(String type)
+    {
+        int result = -1;
+        Random random = new Random();
+
+        if (type.equalsIgnoreCase("row"))
+        {
+            result = random.nextInt(world.numRow);
+        }
+        else if (type.equalsIgnoreCase("col"))
+        {
+            result = random.nextInt(world.numColumn);
+        }
+        else if (type.equalsIgnoreCase("guess"))
+        {
+            result = random.nextInt(tmp.size());
+        }
+
+
+        return result;
+    }
 
 } // end of class RandomGuessPlayer
