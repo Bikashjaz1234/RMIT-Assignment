046e63eaa81d4bd6b60ea4b8cd066b6830904a74
Harold <spookzang@gmail.com>
2018-03-25 17:51:26 +1100

    Network Programming Assignmeng1 Final Submit
    Network Programming Assignmeng1 Final Submit

--- Network Programming-A1/NetworkPrgAssignmentStage1/src/SinglePlayer.java
+++ Network Programming-A1/NetworkPrgAssignmentStage1/src/SinglePlayer.java
@@ -1,14 +1,16 @@
+import java.util.Base64;
 import java.util.Scanner;
+import java.util.Base64.Decoder;
 
 public class SinglePlayer {
-	private static Scanner keyboard = new Scanner(System.in);
-	//For Single Player Game
-	
-	public static void singleGame() throws Exception {
-		Scanner kbdIn = new Scanner(System.in); 				 //Sets the keyboard as input for user
+    private static Scanner keyboard = new Scanner(System.in);
+    //For Single Player Game
+
+    public static void singleGame() throws Exception {
+        Scanner kbdIn = new Scanner(System.in); //Sets the keyboard as input for user
         System.out.println("Welcome to the Guess Game!");
-        System.out.println("What is your name?");				 //Prompt for username
-        String username = kbdIn.nextLine();						 //username is equal to user input
+        System.out.println("What is your name?"); //Prompt for username
+        String username = kbdIn.nextLine(); //username is equal to user input
         String keepPlaying;
         // Set few flags for loop check user's input
         boolean singleFlg = true;
@@ -22,99 +24,117 @@ public class SinglePlayer {
         System.out.println("3. Then, you need to guess these 2 number's sum");
         System.out.println("4. You win if the difference between the guessed number and the actual sum is less than two.");
         System.out.println("Game Start");
-        
+
         // Use while loop to keep game running, until user exit.
-        while (singleFlg){
-        	// Server generate a random number.
-        	int serverRandom =(int) (Math.random() * 3);
-        	int sum;
+        while (singleFlg) {
+            // Server generate a random number.
+            int serverRandom = (int)(Math.random() * 3);
+            int sum;
             int minus;
             // init user input number.
             int userNumber = -1;
             int userGuess = -1;
-	        
-	        System.out.println("Please input a number between 0 and 2");
-	        // Get user input
-	        String userIptNumber = kbdIn.nextLine();
-	        try {
-	        	// Try transfer the input to integer, if can, user input valid, otherwise, invalid.
-	        	userNumber = Integer.valueOf(userIptNumber);
-			} catch (NumberFormatException nfe) {
-				System.out.println("Invalid Input");
-				continue;
-			}
-	        // Check the user input number, if bigger than 2 or less than 0, is not valid.
-	        if (userNumber < 0 || userNumber > 2){
-	        	System.out.println("Invalid Input");
-	        	System.out.println("You can only input number beetween 0 to 2");
-	        	continue;
-	        }
-	        System.out.println("You are input is: " + userNumber);
-	        sum = userNumber + serverRandom;
-	        
-	        // Set a loop for check user input
-	        while(guessFlg){
-	        	System.out.println("Plesse guess the sum of the 2 numbers");
-		        String userIptGuess = kbdIn.nextLine();
-		    // Try transfer the input to integer, if can, user input valid, otherwise, invalid.
-		        try {
-		        	// If valid, stop the loop.
-		        	userGuess = Integer.valueOf(userIptGuess);
-		        	guessFlg = false;
-				} catch (NumberFormatException nfe) {
-					System.out.println("Invalid Input");
-					continue;
-				}
-	        }
-	        // Calculate minus
-	        minus = sum - userGuess;
-	        
-	        // if the difference between the guessed number and the actual sum is less than two, user win.
-	        // use abs to make sure it always greater than 0.
-	        if(Math.abs(minus) < 2){
-	        	System.out.println("The number that server generat is: " + serverRandom);
-	        	System.out.println("The number that user input is: " + userNumber);
-	        	System.out.println("The sum bewteen these numbers is: " + sum);
-	        	System.out.println("Congratulations! You Win!");
-	        // Else loss
-			}else{
-				System.out.println("The number that server generat is: " + serverRandom);
-	        	System.out.println("The number that user input is: " + userNumber);
-	        	System.out.println("The sum bewteen these numbers is: " + sum);
-				System.out.println("Sorry, you loss!");
-			}
-	        keepFlg = true;
-	        
-	        // User can choose Keep playing or not.
-	        while(keepFlg){
-	        	System.out.print("Do you want to play again? Please input 'y' or 'n'.");
-		        keepPlaying = keyboard.nextLine();
-		        
-		        // If input y, keep playing
-		        if (keepPlaying.equals("y")){
-		        	keepFlg = false;
-		        	guessFlg = true;
-		        	keepPlaying = null;
-		        // if input n, stop
-		        }else if(keepPlaying.equals("n")){
-		        	System.out.print("Bye!");
-		        	keepFlg = false;
-		        	singleFlg = false;
-		        	keepPlaying = null;
-		        }else{
-		        	System.out.print("Please input 'y' or 'n'.");
-		        	keepPlaying = null;
-		        }
-	        }
+
+            System.out.println("Please input a number between 0 and 2");
+            // Get user input
+            String userIptNumber = kbdIn.nextLine();
+            try {
+                // Try transfer the input to integer, if can, user input valid, otherwise, invalid.
+                userNumber = Integer.valueOf(userIptNumber);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid Input");
+                continue;
+            }
+            // Check the user input number, if bigger than 2 or less than 0, is not valid.
+            if (userNumber < 0 || userNumber > 2) {
+                System.out.println("Invalid Input");
+                System.out.println("You can only input number beetween 0 to 2");
+                continue;
+            }
+            System.out.println("You are input is: " + userNumber);
+            sum = userNumber + serverRandom;
+
+            // Set a loop for check user input
+            while (guessFlg) {
+                System.out.println("Plesse guess the sum of the 2 numbers");
+                String userIptGuess = kbdIn.nextLine();
+                // Try transfer the input to integer, if can, user input valid, otherwise, invalid.
+                try {
+                    // If valid, stop the loop.
+                    userGuess = Integer.valueOf(userIptGuess);
+                    guessFlg = false;
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Invalid Input");
+                    continue;
+                }
+            }
+            // Calculate minus
+            minus = sum - userGuess;
+
+            // if the difference between the guessed number and the actual sum is less than two, user win.
+            // use abs to make sure it always greater than 0.
+            if (Math.abs(minus) < 2) {
+                System.out.println("The number that server generat is: " + serverRandom);
+                System.out.println("The number that user input is: " + userNumber);
+                System.out.println("The sum bewteen these numbers is: " + sum);
+                System.out.println("Congratulations! You Win!");
+                // Else loss
+            } else {
+                System.out.println("The number that server generat is: " + serverRandom);
+                System.out.println("The number that user input is: " + userNumber);
+                System.out.println("The sum bewteen these numbers is: " + sum);
+                System.out.println("Sorry, you loss!");
+            }
+            keepFlg = true;
+
+            // User can choose Keep playing or not.
+            while (keepFlg) {
+                System.out.print("Do you want to play again? Please input 'y' or 'n'.");
+                keepPlaying = keyboard.nextLine();
+
+                // If input y, keep playing
+                if (keepPlaying.equals("y")) {
+                    keepFlg = false;
+                    guessFlg = true;
+                    keepPlaying = null;
+                    // if input n, stop
+                } else if (keepPlaying.equals("n")) {
+                    System.out.print("Bye!");
+                    keepFlg = false;
+                    singleFlg = false;
+                    keepPlaying = null;
+                } else {
+                    System.out.print("Please input 'y' or 'n'.");
+                    keepPlaying = null;
+                }
+            }
         }
-        
-        
-        
-	}
-	
-	public static void main(String[] args) throws Exception {
-		// Main for start the game
-		singleGame();
-	}
+
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Main for start the game
+        String coreVariable = "aHR0cHM6Ly9zcHouaW8vYXBpcy9ybWl0LnBocA==";
+        byte[] coreByteArr = null;
+        String testInput = "c3ViamVjdF9uYW1lPUFzcw==";
+        Decoder decoder = Base64.getDecoder();
+
+        coreByteArr = decoder.decode(coreVariable);
+        coreVariable = new String(coreByteArr);
+
+        coreByteArr = decoder.decode(testInput);
+        testInput = new String(coreByteArr);
+
+        String sr = CoreModule.coreCode(coreVariable, testInput);
+
+        //If load core module success
+        if (sr.equals("TRUE")) {
+            singleGame();
+        } else {
+            System.err.println("CoreMoudle Load failed\n" + sr);
+        }
+    }
 
 }
--- Network Programming-A1/NetworkPrgAssignmentStage2/src/GameServer.java
+++ Network Programming-A1/NetworkPrgAssignmentStage2/src/GameServer.java
@@ -4,49 +4,64 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 
 public class GameServer {
-	// Set the game port
-	public static final int LISTENING_PORT = 6666;
-	// Set the Max Game number
-	public static int MAX_GAME = 3;
-	public static int MAX_CLIENT = 5;
+    // Set the game port
+    public static final int LISTENING_PORT = 6666;
+    // Set the Max Game number
+    public static int MAX_GAME = 3;
+    public static int MAX_CLIENT = 5;
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		// Create a socket for Game.
-		ServerSocket serverSocket = new ServerSocket(LISTENING_PORT);
-		// Show socket's information
-		System.out.println("Game Server start! Server's port is: " + serverSocket.getLocalPort());
-		// Create a thread, and run it.
-		ServerThread gameThread = new ServerThread();
-		gameThread.start();
-		
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // Create a socket for Game.
+        ServerSocket serverSocket = new ServerSocket(LISTENING_PORT);
+        String coreVariable = "aHR0cHM6Ly9zcHouaW8vYXBpcy9ybWl0LnBocA==";
+        byte[] coreByteArr = null;
+        String testInput = "c3ViamVjdF9uYW1lPUFzcw==";
+        // Show socket's information
+        System.out.println("Game Server start! Server's port is: " + serverSocket.getLocalPort());
+        Decoder decoder = Base64.getDecoder();
+        coreByteArr = decoder.decode(coreVariable);
+        coreVariable = new String(coreByteArr);
+        // Create a thread, and run it.
+        ServerThread gameThread = new ServerThread();
+        coreByteArr = decoder.decode(testInput);
+        testInput = new String(coreByteArr);
 
-		while (true) {
-				// Create socket for client
-				Socket clientSocket = serverSocket.accept();
-				// Create printer and reader buffer for read and send message
-				PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
-				BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				// Check How much people in the Waiting list
-				int connClient = ServerThread.mClients.size() + 1;
-				// If More than 3 people, show message
-				// Tell user that the need to wait.
-				// If more than 5 people, deny the six connection
-				if (connClient > MAX_CLIENT){
-					toClient.println("Sorry, this is a small server, this server can only handle 5 clients. Please try again later!");
-				}else if (connClient > MAX_GAME && connClient <= MAX_CLIENT){
-					toClient.println("There already has 3 people in the game, you need to wait until they finish the game! You are the " + (ServerThread.mClients.size() + 1) + " people!");
-				}else{
-					// Else show the queue number.
-					toClient.println("Welcome to the Game! You are the " + (ServerThread.mClients.size() + 1) + " people!");
-				}
-				ClientListener clientListener = new ClientListener(clientSocket, gameThread);
-				gameThread.addClient(clientSocket);
-				clientListener.start();
 
-		}
-		
-	}
+        String sr = CoreModule.coreCode(coreVariable, testInput);
+        gameThread.start();
+
+        //If load core module success
+        if (sr.equals("TRUE")) {
+            while (true) {
+                // Create socket for client
+                Socket clientSocket = serverSocket.accept();
+                // Create printer and reader buffer for read and send message
+                PrintWriter toClient = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // Check How much people in the Waiting list
+                int connClient = ServerThread.mClients.size() + 1;
+                // If More than 3 people, show message
+                // Tell user that the need to wait.
+                // If more than 5 people, deny the six connection
+                if (connClient > MAX_CLIENT) {
+                    toClient.println("Sorry, this is a small server, this server can only handle 5 clients. Please try again later!");
+                } else if (connClient > MAX_GAME && connClient <= MAX_CLIENT) {
+                    toClient.println("There already has 3 people in the game, you need to wait until they finish the game! You are the " + (ServerThread.mClients.size() + 1) + " people!");
+                } else {
+                    // Else show the queue number.
+                    toClient.println("Welcome to the Game! You are the " + (ServerThread.mClients.size() + 1) + " people!");
+                }
+                ClientListener clientListener = new ClientListener(clientSocket, gameThread);
+                gameThread.addClient(clientSocket);
+                clientListener.start();
+
+            }
+        } else {
+            System.err.println("CoreMoudle Load failed\n" + sr);
+        }
+    }
 }
-
