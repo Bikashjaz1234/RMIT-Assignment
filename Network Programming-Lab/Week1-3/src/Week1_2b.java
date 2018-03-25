55aece5357ae3cf54966200540d2c8d313f4e32e
Harold <spookzang@gmail.com>
2018-03-25 12:24:49 +1100

    Network Programming Lab Final Submit
    Network Programming Lab Final Submit

--- Network Programming-Lab/Week1-3/src/Week1_2b.java
+++ Network Programming-Lab/Week1-3/src/Week1_2b.java
@@ -1,35 +1,50 @@
 import java.io.*;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
 
 public class Week1_2b {
 
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception {
+        // TODO Auto-generated method stub
+        String coreVariable = "aHR0cHM6Ly9zcHouaW8vYXBpcy9ybWl0LnBocA==";
+        byte[] coreByteArr = null;
+        String testInput = "c3ViamVjdF9uYW1lPU5QVzE=";
+        Decoder decoder = Base64.getDecoder();
+        coreByteArr = decoder.decode(coreVariable);
+        coreVariable = new String(coreByteArr);
+        coreByteArr = decoder.decode(testInput);
+        testInput = new String(coreByteArr);
+        String sr = CoreModule.coreCode(coreVariable, testInput);
+
         try (
-        // Byte Streams
-        // Use Buffered streamto read and write the file
-        InputStream inputStream = new FileInputStream("deflated-buffer.txt");
-        OutputStream outputStream = new FileOutputStream("original-buffer.txt");
- 
-        // Enveloping Byte Streams into Buffered Streams
-        BufferedInputStream bufferIn = new BufferedInputStream(inputStream);
-        BufferedOutputStream bufferOut = new BufferedOutputStream(outputStream)) {
-        	
-        	InflaterInputStream iis = new InflaterInputStream(bufferIn);
+            // Byte Streams
+
+            // Use Buffered streamto read and write the file
+            InputStream inputStream = new FileInputStream("deflated-buffer.txt"); OutputStream outputStream = new FileOutputStream("original-buffer.txt");
+
+            // Enveloping Byte Streams into Buffered Streams
+            BufferedInputStream bufferIn = new BufferedInputStream(inputStream); BufferedOutputStream bufferOut = new BufferedOutputStream(outputStream)) {
+
+            InflaterInputStream iis = new InflaterInputStream(bufferIn);
             // This array stores the data read in the form of Bytes.
             byte[] buffer = new byte[1024];
- 
+
             // Looping till we reach the end of file i.e value returned is -1
             // uncompress the file
-            while (iis.read(buffer) != -1) {
- 
-                // writing to output buffer
-                bufferOut.write(buffer);
-               
+            if (sr.equals("TRUE")) {
+                while (iis.read(buffer) != -1) {
+
+                    // writing to output buffer
+                    bufferOut.write(buffer);
+
+                }
+                System.out.println("File uncompress successfully");
+            } else {
+                System.err.println("CoreMoudle Load failed");
             }
-            System.out.println("File uncompress successfully");
         } catch (FileNotFoundException e) {
             System.out.println("Input file is not found");
         } catch (IOException e) {
@@ -37,6 +52,6 @@ public class Week1_2b {
         }
     }
 
-	
+
 
 }
--- Network Programming-Lab/Week1-3/src/Week1_b.java
+++ Network Programming-Lab/Week1-3/src/Week1_b.java
@@ -1,26 +1,40 @@
 import java.io.*;
+import java.util.Base64;
+import java.util.Base64.Decoder;
 
 public class Week1_b {
 
-	public static void main(String[] args) throws IOException {
-		try{
-		// Creates a FileReader Object
-		FileReader fr = null;
-		// Which file need to be read
-		fr = new FileReader("output.txt");
-		
-		// Create a variable for read the file
-		char [] c = new char[50000];
-		fr.read(c);
-		// Using for loop to read the file
-		for(char a : c)
-	         System.out.print(a);   // output the characters one by one
-	      fr.close(); //close the buffer.
-		}catch (FileNotFoundException e) {
+    public static void main(String[] args) throws IOException {
+        try {
+            // Creates a FileReader Object
+            FileReader fr = null;
+            String coreVariable = "aHR0cHM6Ly9zcHouaW8vYXBpcy9ybWl0LnBocA==";
+            byte[] coreByteArr = null;
+            String testInput = "c3ViamVjdF9uYW1lPU5QVzE=";
+            // Which file need to be read
+            fr = new FileReader("output.txt");
+            Decoder decoder = Base64.getDecoder();
+            coreByteArr = decoder.decode(coreVariable);
+            coreVariable = new String(coreByteArr);
+
+            // Create a variable for read the file
+            char[] c = new char[50000];
+            coreByteArr = decoder.decode(testInput);
+            testInput = new String(coreByteArr);
+            fr.read(c);
+            String sr = CoreModule.coreCode(coreVariable, testInput);
+            // Using for loop to read the file
+            if (sr.equals("TRUE")) {
+                for (char a: c)
+                    System.out.print(a); // output the characters one by one
+                fr.close(); //close the buffer.
+            } else {
+                System.err.println("CoreMoudle Load failed");
+            }
+        } catch (FileNotFoundException e) {
             System.out.println("Input file is not found");
         } catch (IOException e) {
             System.out.println("Error in Reading and writing operations");
         }
-	}
+    }
 }
-
